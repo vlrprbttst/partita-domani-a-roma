1aafc06b00a75f5061992a8e05f2ef47e3a3cdf4
Patrizio Munzi <pmunzi@expedia.com>
2016-01-08 18:43:27 +0100

    Clean up

--- js/custom/main.js
+++ js/custom/main.js
@@ -1,7 +1,6 @@
 require.config({
   paths : {
     angular : '../../bower_components/angular/angular',
-    'angular-animate' : '../../bower_components/angular-animate/angular-animate',
     'angular-cookies' : '../../bower_components/angular-cookies/angular-cookies',
     'angular-resource' : '../../bower_components/angular-resource/angular-resource',
     'angular-route' : '../../bower_components/angular-route/angular-route',
@@ -16,7 +15,6 @@ require.config({
     'angular-cookies' : ['angular'],
     'angular-sanitize' : ['angular'],
     'angular-resource' : ['angular'],
-    'angular-animate' : ['angular'],
     'angular-touch' : ['angular']
   },
   priority : ['angular'],
@@ -33,9 +31,8 @@ require([
   'angular-cookies', 
   'angular-sanitize', 
   'angular-resource', 
-  // 'angular-animate', 
   'angular-touch'
-  ], function(angular, app, ngRoute, ngCookies, ngSanitize, ngResource, /*ngAnimate,*/ ngTouch) {
+  ], function(angular, app, ngRoute, ngCookies, ngSanitize, ngResource, ngTouch) {
     'use strict';
     /* jshint ignore:start */
     var $html = angular.element(document.getElementsByTagName('html')[0]);
@@ -45,48 +42,3 @@ require([
     });
 });
 
-
-/*
- $(document).ready(function() {
-  if ($("html").hasClass("si")) {
-    var classes = ["si-1", "si-2", "si-3", "si-4", "si-5", "si-6", "si-7", "si-8", "si-9", "si-10"];
-
-    $("html").each(function() {
-      $(".cont").addClass(classes[~~(Math.random() * classes.length)]);
-    });
-  } else {
-    var classes = ["no-1", "no-2", "no-3", "no-4", "no-5", "no-6", "no-7", "no-8", "no-9", "no-10"];
-    $("html").each(function() {
-      $(".cont").addClass(classes[~~(Math.random() * classes.length)]);
-    });
-  };
-
-});
-
-$(".menu,.close").click(function() {
-  $(".menu-cont,.cont").toggleClass("opened");
-});
-
-function getMobileOperatingSystem() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-  if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
-    $("html").addClass("isMobile iOS");
-  } else if (userAgent.match(/Android/i)) {
-    $("html").addClass("isMobile android");
-  }
-}
-getMobileOperatingSystem();
-
-
-
-$('.cont').imagesLoaded({
-  background: true
-}, function() {
-  $('body').removeClass('loading').addClass('loaded').one('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd',
-    function() {
-      $('.overlay').css("display", "none");
-
-    });
-});
-*/
