c1e552caea7834ce50a7a89020e2016f67bed4bf
Valerio Pierbattista <omegaiori@yahoo.it>
2016-01-09 12:36:07 +0100

    fix css

---
+++ _site/js/app.js
@@ -0,0 +1,83 @@
+/*jshint unused: vars */
+define([
+        'angular',
+        'controllers/page',
+        'controllers/content',
+        'services/matchesprovider',
+        'services/match'
+    ],
+    /*deps*/
+    function(angular, PageCtrl, ContentCtrl) /*invoke*/ {
+        'use strict';
+
+        /**
+         * @ngdoc overview
+         * @name wutsapp
+         * @description
+         * # wutsapp
+         *
+         * Main module of the application.
+         */
+        return angular
+            .module('partitaaroma', [
+                'partitaaroma.controllers.PageCtrl',
+                'partitaaroma.controllers.ContentCtrl',
+                'partitaaroma.services.MatchesProvider',
+                'partitaaroma.services.Match',
+                'ngCookies',
+                'ngResource',
+                'ngSanitize',
+                'ngRoute',
+                //                'ngAnimate',
+                'ngTouch'
+            ])
+            .config(function($routeProvider, $locationProvider) {
+                $locationProvider.html5Mode(true);
+                $routeProvider
+                    .when('/', {
+                        templateUrl: 'views/content.html',
+                        controller: 'ContentCtrl',
+                        resolve: {
+                            match: function($route, MatchesProvider) {
+                                return MatchesProvider.getMatch(1);
+                            }
+                        }
+                    })
+                    .when('/oggi', {
+                        templateUrl: 'views/content.html',
+                        controller: 'ContentCtrl',
+                        resolve: {
+                            match: function($route, MatchesProvider) {
+                                return MatchesProvider.getMatch(0);
+                            }
+                        }
+                    })
+                    .when('/si', {
+                        templateUrl: 'views/content.html',
+                        controller: 'ContentCtrl',
+                        resolve: {
+                            match: function($route, MatchesProvider, Match) {
+                                var json = {
+                                    timestamp: new Date().getTime(),
+                                    homeTeamName: "AS Pierba",
+                                    awayTeamName: "",
+                                };
+
+                                return new Match(json);
+                            }
+                        }
+                    })
+                    .when('/no', {
+                        templateUrl: 'views/content.html',
+                        controller: 'ContentCtrl',
+                        resolve: {
+                            match: function($route, MatchesProvider, Match) {
+                                return null;
+                            }
+                        }
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    });
+            });
+    });
---
+++ _site/js/controllers/content.js
@@ -0,0 +1,26 @@
+define(['angular', 'services/device'], function(angular) {
+    'use strict';
+
+    /**
+     * @ngdoc function
+     * @name wutsapp.controller:HomeCtrl
+     * @description
+     * # HomeCtrl
+     * Controller of the wutsapp
+     */
+    angular.module('partitaaroma.controllers.ContentCtrl', ['partitaaroma.services.Device'])
+        .controller('ContentCtrl', function($rootScope, $scope, $window,  $location, Device, match) {
+            $scope.link = 'oggi';
+            $scope.date = 'domani';
+            if ($location.path().indexOf('oggi') != -1) {
+                $scope.link = 'domani';
+                $scope.date = 'oggi';
+            }
+
+            $scope.match = match;
+            $scope.contentClass = ($scope.match ? 'si' : 'no') + '-' + Math.floor((Math.random() * 10) + 1);
+
+            $rootScope.loaded = true;
+
+        });
+});
