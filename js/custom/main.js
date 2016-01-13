4b6b5389cb4db4583cd4fff1b9a76e177ae96f4f
Valerio Pierbattista <omegaiori@yahoo.it>
2016-01-13 20:17:32 +0100

    social btns

--- js/custom/main.js
+++ js/custom/main.js
@@ -42,7 +42,7 @@ require([
     });
 });
 
-  
+
   //google analytics
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -52,3 +52,32 @@ require([
 
   ga('create', 'UA-72386870-1', 'auto');
   ga('send', 'pageview');
+
+  // fb
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  //twt
+
+  window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
