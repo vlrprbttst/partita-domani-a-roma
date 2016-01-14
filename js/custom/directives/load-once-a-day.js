c43014a1467e35edd67fc1f86781395b097b437d
Patrizio Munzi <pmunzi@expedia.com>
2016-01-14 18:21:26 +0100

    Fixed once-a-day-reload feature

--- js/custom/directives/load-once-a-day.js
+++ js/custom/directives/load-once-a-day.js
@@ -2,21 +2,28 @@ define(['angular'], function(angular) {
     'use strict';
 
     angular.module('partitaaroma.directives.LoadOnceADay', [])
-        .directive("loadOnceADay", function($route, $timeout) {
+        .directive("loadOnceADay", function($route, $interval) {
             return {
                 restrict: 'A',
                 scope: true,
                 link: function(scope, element, attrs) {
-                    var nowMillis = new Date().getTime();
-                    var tomorrow = new Date(nowMillis + 86400*1000);
+                    var tomorrow = new Date((new Date().getTime()) + 86400*1000);
                     tomorrow.setHours(0);
-                    tomorrow.setMinutes(1);
+                    tomorrow.setMinutes(0);
                     tomorrow.setSeconds(0);
                     var tomorrowMillis = tomorrow.getTime();
-                    var reloadDelay = tomorrowMillis - nowMillis;
-                    $timeout(function() {
-                        $route.reload();
-                    }, reloadDelay);
+                    $interval(function() {
+                        var nowMillis = new Date().getTime();
+                        if(nowMillis > tomorrowMillis) {
+                            tomorrow = new Date((new Date().getTime()) + 86400*1000);
+                            tomorrow.setHours(0);
+                            tomorrow.setMinutes(0);
+                            tomorrow.setSeconds(0);
+                            tomorrowMillis = tomorrow.getTime();
+
+                            $route.reload();                        
+                        }
+                    }, 500);
                 }
             };
         });
