38e625896ee1f5f68248d929397566b56cba7711
Patrizio Munzi <pmunzi@expedia.com>
2016-02-02 14:38:58 +0100

    Fixed social buttons loading. Moved icons related resources to icons folder

--- js/custom/controllers/cookies.js
+++ js/custom/controllers/cookies.js
@@ -9,10 +9,19 @@ define(['angular', 'services/utilities'], function(angular) {
      * Controller of the partitaaroma
      */
     angular.module('partitaaroma.controllers.CookiesCtrl', ['partitaaroma.services.Utilities'])
-        .controller('CookiesCtrl', function($rootScope, $scope, Utilities) {
+        .controller('CookiesCtrl', function($rootScope, $scope, $window, $cookies, Utilities) {
             $scope.headerImage = Utilities.loadImage('/images/social-img.jpg', function() {
                 $rootScope.loaded = true;
                 $rootScope.$apply();
             });
+
+            $scope.back = function() {
+                var cookieExpirationDate = new Date();
+                cookieExpirationDate.setYear(2100);
+                $cookies.put('showCookiesPolicy', 'false', {
+                    expires: cookieExpirationDate
+                });
+                $window.history.back();
+            };
         });
 });
