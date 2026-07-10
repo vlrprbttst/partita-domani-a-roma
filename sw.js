469d59191b0f7c12bcd457b597c0b36065ffe177
vlrprbttst <vlrprbttst@users.noreply.github.com>
2026-07-10 09:24:58 +0000

    deploy: 5f171eeeb851b34c05ef0e489348786be9fd6b82

--- sw.js
+++ sw.js
@@ -7,4 +7,4 @@ self.addEventListener('activate', e => e.waitUntil(clients.claim()))
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request, { cache: 'no-store' }).catch(() => caches.match(e.request).then(r => r ?? Response.error())))
 })
-// build 2026-07-10T02:28:59Z
+// build 2026-07-10T09:24:56Z
